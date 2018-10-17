ade8be35c450d020c274b4b050e2c83cc1b9d2df
Santosh <santosh.poudyal@gmail.com>
2018-10-17 16:08:53 +1100

    added authentication key in new.html and updated books_controller.rb

--- FormLearning/app/controllers/books_controller.rb
+++ FormLearning/app/controllers/books_controller.rb
@@ -24,17 +24,20 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    # @book = Book.new(book_params)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+    # respond_to do |format|
+    #   if @book.save
+    #     format.html { redirect_to @book, notice: 'Book was successfully created.' }
+    #     format.json { render :show, status: :created, location: @book }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @book.errors, status: :unprocessable_entity }
+    #   end
+    # end
+
+    @params = params[:sales]
+    render 'new'
   end
 
   # PATCH/PUT /books/1
